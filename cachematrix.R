75991cbdc0aec788e8091fe115138f9d937da0e6
DSSfidd <fiddie.14@gmail.com>
2015-08-23 12:16:01 +0200

    proba 1

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# This function creates a list containing data matrix and its cached inverse
+# furthermore the list contains setinv and getinv functions which
+# set and return the calculated inverse
 
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) i <<- solve
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
+# This function checks whether the inverse of a matrix has already been solved
+# in case it has not, it calculates the inverse and caches it
+# finally the inverse is returned
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
